c916e120ff08ae5726b5b4b29b8d95059f1e9343
Josh Brito <jbrito6492@gmail.com>
2021-02-12 21:25:32 -0700

    added keyframes to image gallery

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,31 @@
-import { Img, Fade, useDisclosure } from "@chakra-ui/react";
+import {
+  Img,
+  Fade,
+  useDisclosure,
+  keyframes,
+  usePrefersReducedMotion,
+} from "@chakra-ui/react";
 import headerStyles from "../styles/Header.module.css";
 
 const Header = () => {
   const { onOpen } = useDisclosure();
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  const shrink = keyframes`
+  from { transform: scale(1); }
+  to { transform: scale(.5); }
+  `;
+
+  const animation = prefersReducedMotion
+    ? undefined
+    : `${shrink} 1.5s linear normal forwards`;
+
   return (
     <div>
       <Fade in={onOpen}>
-        <h1 className={headerStyles.title}>
-          <Img src="logo.png" alt="Diovici Logo" />
-        </h1>
+        <div className={headerStyles.title}>
+          <Img src="logo.png" alt="Diovici Logo" animation={animation} />
+        </div>
         <p className={headerStyles.description}>Pants to fit your lifestyle</p>
       </Fade>
     </div>
--- components/ImageGallery.js
+++ components/ImageGallery.js
@@ -5,24 +5,38 @@ const ImageGallery = () => {
   const prefersReducedMotion = usePrefersReducedMotion();
   const fadeIn = keyframes`
   0% { opacity: 0; }
+
   100% { opacity: 1; }
+  `;
+
+  const move = keyframes`
+  0% { opacity: 0; bottom: 0; }
+  100% { opacity: 1; bottom: 30rem; }
   `;
 
   const fadeInAnimation = prefersReducedMotion
     ? undefined
-    : `${fadeIn} 1s linear normal forwards`;
+    : `${fadeIn} 1.5s linear normal forwards`;
+
+  const moveAnimation = prefersReducedMotion
+    ? undefined
+    : `${move} 1.5s linear normal forwards`;
 
   return (
-    <div className={galleryStyles.gallery}>
-      {[...Array(8)].map((image, index) => (
-        <Img
-          key={index}
-          animation={fadeInAnimation}
-          objectFit="scale-down"
-          src={`galleryimg${index + 1}.jpg`}
-          className={`${galleryStyles.img} ${galleryStyles.num}${index + 1}}`}
-        />
-      ))}
+    <div className={`${galleryStyles.gallery}`}>
+      {[...Array(8)].map((image, index) => {
+        const idx = [0, 3, 4, 7];
+        const animation = idx.includes(index) ? moveAnimation : fadeInAnimation;
+        return (
+          <Img
+            key={index}
+            animation={animation}
+            objectFit="scale-down"
+            src={`galleryimg${index + 1}.jpg`}
+            className={`${galleryStyles.item} ${galleryStyles.img}${index + 1}`}
+          />
+        );
+      })}
     </div>
   );
 };
